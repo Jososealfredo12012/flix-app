932539b382e8e3488fab7bee999139500b09f2d7
Jose Hernandez <josealfredo.066@gmail.com>
2025-05-19 12:51:26 -0400

    Implement display of popular movies in the UI

--- js/script.js
+++ js/script.js
@@ -2,14 +2,63 @@ const global = {
   currentPage: window.location.pathname,
 };
 
+async function displayPopularMovies() {
+  const { results } = await fetchAPIData("movie/popular");
+  
+  results.forEach(movie =>{
+    const div = document.createElement('div');
+    div.classList.add('card')
+    div.innerHTML = `
+    
+          <a href="movie-details.html?id=${movie.id}">
+            ${
+                movie.poster_path 
+                ? `<img
+              src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
+              class="card-img-top"
+              alt="Movie Title"
+            />` : `<img
+              src="images/no-image.jpg"
+              class="card-img-top"
+              alt="Movie Title"
+            />`
+            }
+          </a>
+          <div class="card-body">
+            <h5 class="card-title">${movie.title}</h5>
+            <p class="card-text">
+              <small class="text-muted">Release: ${movie.release_date}</small>
+            </p>
+          </div>
+
+    `
+    document.querySelector('#popular-movies').appendChild(div)
+  })
+}
+
+// Fetch data from TMDB API
+
+async function fetchAPIData(endpoint) {
+  const API_KEY = "8e7d746f2a9457f54fce3af872443aea";
+  const API_URL = " https://api.themoviedb.org/3/";
+
+  const response = await fetch(
+    `${API_URL}${endpoint}?api_key=${API_KEY}&=en-US`
+  );
+
+  const data = await response.json();
+
+  return data;
+}
+
 // highlight active link
 function highlightActiveLink() {
-    const links = document.querySelectorAll('.nav-link')
-    links.forEach(link => {
-        if (link.getAttribute('href') === global.currentPage) {
-            link.classList.add('active')
-        }
-    })
+  const links = document.querySelectorAll(".nav-link");
+  links.forEach((link) => {
+    if (link.getAttribute("href") === global.currentPage) {
+      link.classList.add("active");
+    }
+  });
 }
 
 //init app
@@ -18,7 +67,7 @@ function init() {
   switch (global.currentPage) {
     case "/":
     case "/index.html":
-      console.log("home");
+      displayPopularMovies();
       break;
     case "/shows.html":
       console.log("Shows");
@@ -36,7 +85,7 @@ function init() {
       break;
   }
 
-  highlightActiveLink()
+  highlightActiveLink();
 }
 
 document.addEventListener("DOMContentLoaded", init);
